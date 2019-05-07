0e48c04ec88ba4d6ba9a95d89fc64f3385b8aa37
guiguiplot <bguinaudeau@gmail.com>
2019-05-07 18:54:53 +0200

    add a bunch of functions

--- R/main.R
+++ R/main.R
@@ -1,10 +1,10 @@
 #' sudo$
 #' @description This function executes the given command as sudo
-#' @param cmd A bash command to execute as sudo
+#' @param command A bash command to execute as sudo
 #' @param env_var Should the password be saved for further use
 #' @export
 
-sudo <- function(cmd,
+sudo <- function(command,
                  intern = F,
                  ignore.stdout = F,
                  ignore.stderr = F,
@@ -14,7 +14,7 @@ sudo <- function(cmd,
     keyring::key_set("SUDO_PASS")
   }
 
-  out <- system(glue::glue("echo { keyring::key_get('SUDO_PASS') } | sudo -S { cmd }"),
+  out <- exec(glue::glue("echo { keyring::key_get('SUDO_PASS') } | sudo -S { command }"),
          intern = intern,
          ignore.stdout = ignore.stdout,
          ignore.stderr = ignore.stderr)
@@ -22,5 +22,34 @@ sudo <- function(cmd,
   if(!env_var) keyring::key_delete("SUDO_PASS")
 
   return(out)
+}
+
+#' exec
+#' @export
+
+exec <- function(string, cmd = F, ...) if(cmd == T) return(string) else return(system(string, ...))
+
+#' current_user
+#' @export
+
+current_user <- function() system("echo $USER", intern = T)
+
+#' source_rscript
+#' @export
+
+source_rscript <- function(path, cmd = F) exec(glue::glue("Rscript { path }"), cmd = cmd)
+
+#' append
+#' @export
+
+append <- function(path, string, cmd = F) exec(glue::glue("echo { string } >> { path }"), cmd = cmd)
+
+#' chmod
+#' @export
+
+chmod <- function(path, right, recursive = NULL, cmd, ...){
+  recursive <- ifelse(is.null(recursive), "", recursive)
+
+  exec(glue::glue("chmod { recursive } { right } { path }"), cmd = cmd, ...)
 }
 
